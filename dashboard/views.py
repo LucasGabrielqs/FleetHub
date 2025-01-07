1adcf8e4f9e15e9991484fc0e509bfaadddd12f9
Lucas Gabriel de Queiroz Souza <lucasgabriel20032013@gmail.com>
2025-01-06 21:18:39 -0300

    Ajustes gerais

--- dashboard/views.py
+++ dashboard/views.py
@@ -26,9 +26,22 @@ def recuperar_senha(request):
 def cadastro_usuario(request):
     return render(request,'dashboard/cadastro_usuario_app.html')
 
+def listagem_usuarios(request):
+    contexto = {
+        'range_10': range(10),
+        'range_7': range(7),  # Lista de 0 a 6
+        'range_4': range(4),  # Lista de 0 a 3
+        'range_2': range(2)  # Lista de 0 a 1
+    }
+    return render(request, 'dashboard/listagem_usuarios.html',contexto)
+
+def informacoes_usuario(request):
+    return render(request, 'dashboard/informacoes_usuarios.html')
+
+
+
 def agendar_manutencao(request):
     return render(request,'dashboard/agendar_manutencao.html')
-
 
 def listagem_manutencao(request):
     contexto = {
@@ -42,35 +55,59 @@ def listagem_manutencao(request):
 def editar_manutencao(request):
     return render(request,'dashboard/editar_manutencao.html')
 
+
+
 def criar_reserva(request):
     return render(request, 'dashboard/criacao_reserva.html')
 
 def editar_reserva(request):
     return render(request, 'dashboard/editar_reserva.html')
 
-def listagem_usuarios(request):
+def listagem_reservas(request):
     contexto = {
         'range_10': range(10),
         'range_7': range(7),  # Lista de 0 a 6
         'range_4': range(4),  # Lista de 0 a 3
         'range_2': range(2)  # Lista de 0 a 1
     }
-    return render(request, 'dashboard/listagem_usuarios.html',contexto)
+    return render(request, 'dashboard/listagem_reservas.html',contexto)
+
 
 def criacao_rota(request):
     return render(request, 'dashboard/criacao_rota.html')
 
+def listagem_rota(request):
+    contexto = {
+        'range_10': range(10),
+        'range_7': range(7),  # Lista de 0 a 6
+        'range_4': range(4),  # Lista de 0 a 3
+        'range_2': range(2)  # Lista de 0 a 1
+    }
+    return render(request,'dashboard/listagem_rota.html',contexto)
+
 def visualizacao_rota(request):
     return render(request, 'dashboard/visualizacao_rota.html')
 
-def informacoes_usuario(request):
-    return render(request, 'dashboard/informacoes_usuarios.html')
 
-def listagem_reservas(request):
-    return render(request, 'dashboard/listagem_reservas.html')
+def criacao_abastecimento(request):
+    return render(request,'dashboard/criacao_abastecimento.html')
+
+def listagem_abastecimento(request):
+    contexto = {
+        'range_10': range(10),
+        'range_7': range(7),  # Lista de 0 a 6
+        'range_4': range(4),  # Lista de 0 a 3
+        'range_2': range(2)  # Lista de 0 a 1
+    }
+    return render(request,'dashboard/listagem_abastecimento.html',contexto)
 
 def registro_abastecimento(request):
     return render(request, 'dashboard/registro_abastecimento.html')
 
-def informacoes_veiculo(request):
-    return render(request, 'dashboard/informacoes_veiculo.html')
+
+
+
+
+# Já existe a parte de veiculos, está em outro app
+# def informacoes_veiculo(request): 
+#     return render(request, 'dashboard/informacoes_veiculo.html')
