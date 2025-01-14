eb00bd623a4a67a110b438c10bdee260cce267f0
Lucas Gabriel de Queiroz Souza <lucasgabriel20032013@gmail.com>
2025-01-13 21:36:09 -0300

    Ajustes Responsividade até reserva

--- dashboard/static/js/info-usuario-script.js
+++ dashboard/static/js/info-usuario-script.js
@@ -1,6 +1,10 @@
 const campos_input = document.getElementsByClassName('input_container');
 const alterar_input_nome = document.getElementById('nome');
 const alterar_input_email = document.getElementById('email');
+const alterar_input_cpf = document.getElementById('cpf');
+const alterar_input_ddd = document.getElementById('select-ddd-inter');
+const alterar_input_telefone = document.getElementById('telefone');
+const alterar_input_status = document.getElementById('estados');
 const rodape = document.getElementById('rodape');
 
 for (let i = 0; i < campos_input.length; i++) {
@@ -11,12 +15,22 @@ for (let i = 0; i < campos_input.length; i++) {
 
 function fazer_alteracoes() {
     alterar_input_email.style.pointerEvents = 'visible';
+    alterar_input_nome.style.pointerEvents = 'visible';
+    alterar_input_cpf.style.pointerEvents = 'visible';
+    alterar_input_ddd.style.pointerEvents = 'visible';
+    alterar_input_telefone.style.pointerEvents = 'visible';
+    alterar_input_status.style.pointerEvents = 'visible';
     rodape.style.display = 'flex'; // Exibe o rodapé
 }
 
 function cancelar_alteracoes(){
     if(confirm('As Informações não Salvas serão Perdidas!')){
         alterar_input_email.style.pointerEvents = 'none';
+    alterar_input_nome.style.pointerEvents = 'none';
+    alterar_input_cpf.style.pointerEvents = 'none';
+    alterar_input_ddd.style.pointerEvents = 'none';
+    alterar_input_telefone.style.pointerEvents = 'none';
+    alterar_input_status.style.pointerEvents = 'none';
         rodape.style.display = 'none';
         //adicionar lógica para recarregar as informações salvas
     }
